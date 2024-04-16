ca273176e63a8578c564d19cd251f812813178a3
MatheusNery005 <164574641+MatheusNery005@users.noreply.github.com>
2024-04-15 22:16:02 -0300

    Add files via upload

---
+++ exercicio2.py
@@ -0,0 +1,6 @@
+lista = []
+for i in range(5)
+    v = int(input("Digite o valor [i + 1] "))
+    print(
+
+for i in range(max)
---
+++ exercicio6.py
@@ -0,0 +1,12 @@
+from random import randnt
+resultados = [0]*7
+print(resultados)
+for_in range(7000):
+ lado = randnt(1,6)
+ resultado[lado]=resultado[lado]+ 1
+
+for i in range(1,7)
+ fred[i] = (resultados[i] / 6000) * 100
+
+for i in range(1,7):
+ print(f"{i} - {resultado[i]}")
